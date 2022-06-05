d088c3d6bee782b467cef3a252a62fcc7bdbaeb6
leoD-E1 <leodangeli11@gmail.com>
2022-06-05 00:50:08 -0300

    [update] added more functions and styles changes

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
 const inputName = document.getElementById("name");
 const inputCost = document.getElementById("cost");
 const submitButton = document.getElementById("submit");
-const listaNombreYmonto = document.querySelector("#nombreYmonto");
+const listLogs = document.getElementById("listLogs");
+const log = document.getElementById("log").content;
+const fragment = document.createDocumentFragment();
 
 let peopleName = "";
 let cost = 0;
 
+const logs = [];
+let totalCost = 0;
+// document.addEventListener("DOMContentLoaded", () => {
+
+// })
+
 inputName.addEventListener("change", (evt) => catchName(evt));
 inputCost.addEventListener("change", (evt) => catchCost(evt));
+submitButton.addEventListener("click", () => execute());
 
 const catchName = (evt) => (peopleName = evt.target.value);
 const catchCost = (evt) => (cost = evt.target.value);
 
-const ejecutar = () => {
-  mostrarNombreYmonto(peopleName, cost);
-  inputName.value = "";
-  inputCost.value = 0;
+const restartValues = () => {
+  inputName.value = null;
+  inputCost.value = null;
+  peopleName = null;
+  cost = null;
 };
 
-function mostrarNombreYmonto(_nombre, _monto) {
-  listaNombreYmonto.innerHTML += `<li class="list-group-item">${_nombre}: $${_monto}</li>`;
-}
+const drawLogs = () => {
+  console.log(logs);
+  logs.map((item) => {
+    log.querySelector("h5").textContent = item.name;
+    log.querySelector("span").textContent = `$${item.cost}`;
+    log.querySelector("button").dataset.id = item.id;
+    const clone = log.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  listLogs.appendChild(fragment);
+};
+
+const execute = () => {
+  if (peopleName && cost !== 0) {
+    try {
+      submitNameAndCost(peopleName, cost);
+      restartValues();
+    } catch (error) {
+      alert(error);
+    }
+  } else {
+    alert(
+      `Nombre es ${peopleName}, costo es ${cost}, ingrese valores correctos.`
+    );
+  }
+};
+
+const submitNameAndCost = (_name, _cost) => {
+  logs.push({ id: Math.random, name: _name, cost: _cost });
+  drawLogs();
+  // listLogs.innerHTML += `<li class="list-group-item">${_name}: ${_cost}</li>`;
+};
